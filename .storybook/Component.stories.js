4bf36943f33b6c79c31b71ee844ea1f2418441e5
Juan Pablo Mejia <juanpmd@hotmail.com>
2022-12-14 12:52:57 -0500

    change tabs for spaces

--- .storybook/Component.stories.js
+++ .storybook/Component.stories.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { CounterComponent } from '../src/index.js';
 
 export default {
-	component: CounterComponent,
-	title: 'Counter'
+  component: CounterComponent,
+  title: 'Counter'
 };
 
 export const Placeholder = () => <CounterComponent />;
--- rollup.config.js
+++ rollup.config.js
@@ -5,20 +5,20 @@ import terser from '@rollup/plugin-terser';
 import pkg from './package.json';
 
 export default {
-	input:'src/index.js',
-	output: [
-		{ file: pkg.main, format: 'cjs' },
-		{ file: pkg.module, format: 'esm' }
-	],
-	plugins: [
-		babel({
-			babelHelpers: 'bundled',
-			exclude: 'node_modules/**',
-			presets: ['@babel/preset-env','@babel/preset-react']
-		}),
-		resolve(),
-		commonjs(),
-		terser()
-	],
-	external: Object.keys(pkg.peerDependencies)
+  input:'src/index.js',
+  output: [
+    { file: pkg.main, format: 'cjs' },
+    { file: pkg.module, format: 'esm' }
+  ],
+  plugins: [
+    babel({
+      babelHelpers: 'bundled',
+      exclude: 'node_modules/**',
+      presets: ['@babel/preset-env','@babel/preset-react']
+    }),
+    resolve(),
+    commonjs(),
+    terser()
+  ],
+  external: Object.keys(pkg.peerDependencies)
 };
