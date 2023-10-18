67f41e0b36e7847d705fe5ab9b80d9d4317fcc8e
Juan Pablo Mejia <juanpmd@hotmail.com>
2023-10-18 10:40:17 -0700

    update package.json file import in rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -2,10 +2,10 @@ import babel from '@rollup/plugin-babel';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import terser from '@rollup/plugin-terser';
-import pkg from './package.json';
+import * as pkg from './package.json';
 
 export default {
-  input:'src/index.js',
+  input: 'src/index.js',
   output: [
     { file: pkg.main, format: 'cjs' },
     { file: pkg.module, format: 'esm' }
@@ -18,7 +18,7 @@ export default {
     }),
     resolve(),
     commonjs(),
-    terser()
+    terser(),
   ],
-  external: Object.keys(pkg.peerDependencies)
+  external: Object.keys(pkg.peerDependencies),
 };
