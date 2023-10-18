413319cecb135d3c39ed71960d165344783b0235
Juan Pablo Mejia <juanpmd@hotmail.com>
2023-10-18 15:35:04 -0700

    update dependencies + file formats + engines.node definition

--- rollup.config.js
+++ rollup.config.js
@@ -2,13 +2,13 @@ import babel from '@rollup/plugin-babel';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import terser from '@rollup/plugin-terser';
-import * as pkg from './package.json';
+import pkg from './helpers/load-package.cjs';
 
 export default {
   input: 'src/index.js',
   output: [
     { file: pkg.main, format: 'cjs' },
-    { file: pkg.module, format: 'esm' }
+    { file: pkg.module, format: 'esm' },
   ],
   plugins: [
     babel({
